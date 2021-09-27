aefcefb7b11a549e1089409a33e7a0252819bd60
Marwa <marwama3bade@gmail.com>
2021-09-27 17:39:12 +0100

    Implemented 6 exercises - capaitalize,generateInitials,addVAT,getSalePrice,getMiddleCharacter,reverseWord

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,34 +1,41 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.charAt(0).toUpperCase() + word.substring(1,word.length);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName.substring(0,1)+'.' + lastName.substring(0,1);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  //console.log(((vatRate/100.0)*originalPrice) + originalPrice);
+  let res = ((vatRate/100.00)*originalPrice) + originalPrice;
+  //console.log(Number(res).toFixed(2));
+  return res%1 === 0? res : parseFloat(Number(res).toFixed(2)); //Number(res).toFixed(2) return string need to change to float 
+  //return res%1 == 0? res : res.toPrecision(4);//return string 
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  let res = originalPrice - ((reduction/100.00)*originalPrice);
+  return res%1 === 0? res : parseFloat(Number(res).toFixed(2));
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  let middle = str.length/2 ;
+  //return str.length %2 === 0? str.substring(middle-1,middle)+ str.substring(middle,middle+1):str.substring(middle,middle+1);
+  return str.length %2 === 0? str.charAt(middle-1) + str.charAt(middle):str.charAt(middle);
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split('').reverse().join('');
 }
 
 function reverseAllWords(words) {
