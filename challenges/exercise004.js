33777256c4b2ca4da3a23791d5584d87316677c3
Marwa <marwama3bade@gmail.com>
2021-09-28 13:43:00 +0100

    Implemented exercise004 Using forEach,.map,.filter

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,85 +1,64 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  let res = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 1) {
-      res.push(nums[i]);
-    }
-  }
-  return res;
+  return (nums.filter(num => {
+    return (num < 1);
+  }));
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  let res = [];
-  for (let i = 0; i < names.length; i++) {
-    if (names[i].charAt(0) === char) {
-      res.push(names[i]);
-    }
-  }
-  return res;
+  return (names.filter(name => {
+    return (name.charAt(0) === char);
+  }));
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  let res = [];
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].substring(0, 3) === 'to ') {
-      res.push(words[i]);
-    }
-  }
-  return res;
+  return (words.filter(word => {
+    return (word.substring(0, 3) === 'to ');
+  }));
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  let res = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (Number.isInteger(nums[i])) {
-      res.push(nums[i]);
-    }
-  }
-  return res;
+  return (nums.filter(num => {
+    return (Number.isInteger(num));
+  }));
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   let res = [];
-  for (let i = 0; i < users.length; i++) {
-    res.push(users[i].data.city.displayName);
-  }
+  users.forEach(user => {
+    res.push(user.data.city.displayName);
+  });
   return res;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  let res = [];
-  for (let i = 0; i < nums.length; i++) {
-    let x = Math.sqrt(nums[i]);
-    res.push(x % 1 ? parseFloat(x.toFixed(2)) : x);
-  }
-  return res;
+  return (nums.map(num => {
+    let x = Math.sqrt(num);
+    return (x % 1 ? parseFloat(x.toFixed(2)) : x);
+  }));
+
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  let res = [];
-  for (let i = 0; i < sentences.length; i++) {
-    if (sentences[i].toLowerCase().includes(str.toLowerCase())) {
-      res.push(sentences[i]);
-    }
-  }
-  return res;
+  return (sentences.filter(sentence => {
+    return (sentence.toLowerCase().includes(str.toLowerCase()));
+  }));
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   let res = [];
-  for (let i = 0; i < triangles.length; i++) {
-    res.push(Math.max(...triangles[i]));
-  }
+  triangles.forEach(triangle => {
+    res.push(Math.max(...triangle));
+  });
   return res;
 }
 
