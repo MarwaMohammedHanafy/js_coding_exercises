812f572b79681d09a822e29f37c0a487ade4e35d
Marwa <marwama3bade@gmail.com>
2021-09-29 19:59:07 +0100

    Implemented exercise006

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -6,37 +6,89 @@
  */
 const sumMultiples = arr => {
   if (arr === undefined) throw new Error("arr is required");
+  if (!Array.isArray(arr)) throw new Error("arr must be an Array");
+  let total = 0;
+  arr.forEach(n => {
+    if (n % 5 === 0 || n % 3 === 0) {
+      total += n;
+    }
+  });
+  return total;
 };
 
 /**
- * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.
+ * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string.
+ *  A valid DNA string may contain characters C, G, T or A only.
  * @param {String} str
  * @returns {Boolean}
  */
 const isValidDNA = str => {
   if (str === undefined) throw new Error("str is required");
+  str = str.toLowerCase();
+  let check = 'CGTAcgta';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (!check.includes(char)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /**
- * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
+ * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. 
+ * In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
  * @param {String} str
  * @returns {String}
  */
 const getComplementaryDNA = str => {
   if (str === undefined) throw new Error("str is required");
+  if (!isValidDNA(str)) throw new Error("str is not a valid DNA string");
+  let basePair = '';
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    switch (char) {
+      case 'c': case 'C':
+        basePair += 'G'
+        break;
+      case 'g': case 'G':
+        basePair += 'C'
+        break;
+      case 'a': case 'A':
+        basePair += 'T'
+        break;
+      case 't': case 'T':
+        basePair += 'A'
+        break;
+    }
+  }
+  return basePair;
 };
 
 /**
- * This function should receive a number and return true/false depending on whether it is a prime number or not. A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
+ * This function should receive a number and return true/false depending on whether it is a prime number or not.
+ *  A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
  * @param {Number} n
  * @returns {Boolean}
  */
 const isItPrime = n => {
   if (n === undefined) throw new Error("n is required");
+  if (!Number.isInteger(n)) throw new Error("n is not an integer");
+  if (n < 0) throw new Error("negative integers can not be prime");
+  if (n === 0) throw new Error("zero is not prime");
+  if (n === 1) throw new Error("1 is not prime");
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+
 };
 
 /**
- * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
+ * This function should receive a number and return an array of n arrays, each filled with n items.
+ *  The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
  * [
  *   ["foo", "foo", "foo"],
  *   ["foo", "foo", "foo"],
@@ -46,9 +98,23 @@ const isItPrime = n => {
  * @param {Any} fill
  * @returns {Array}
  */
-const createMatrix = (n, fill) => {
+const createMatrix = (n  , fill) => {
+ // console.log(n)
+ // console.log(fill)
   if (n === undefined) throw new Error("n is required");
   if (fill === undefined) throw new Error("fill is required");
+  
+  let resArray = [];
+  for (let i = 0; i < n; i++) {
+    resArray[i] = [];
+    for (let j = 0; j < n; j++) {
+     // console.log(j);
+      resArray[i][j] = fill;
+    }
+    //console.log(i);
+  }
+  //console.log(resArray);
+return resArray;
 };
 
 /**
@@ -66,6 +132,16 @@ const createMatrix = (n, fill) => {
 const areWeCovered = (staff, day) => {
   if (staff === undefined) throw new Error("staff is required");
   if (day === undefined) throw new Error("day is required");
+  if(staff === 0 ){
+    return false;
+  }
+  let countOfStaff = 0; 
+  for (let key in staff){
+    if(staff[key].rota.includes(day)){
+      countOfStaff++;
+    }
+  }
+  return countOfStaff >= 3 ? true: false;
 };
 
 module.exports = {
---
+++ test/exercise006.test.js
@@ -0,0 +1,159 @@
+const {
+    sumMultiples,
+    isValidDNA,
+    getComplementaryDNA,
+    isItPrime,
+    createMatrix,
+    areWeCovered
+} = require("../challenges/exercise006");
+
+describe("sumMultiples", () => {
+    test('it throws an error if not passed array ', () => {
+        expect(() => {
+            sumMultiples();
+        }).toThrow("arr is required");
+        // expect(sumMultiples()).toThrow("arr is required");why this not working ??
+        expect(() => {
+            sumMultiples("foo");
+        }).toThrow("arr must be an Array");
+        expect(() => {
+            sumMultiples(4);
+        }).toThrow("arr must be an Array");
+    });
+
+    test('it works okay with decimal numbers', () => {
+        expect(sumMultiples([1, 3, 5.0, 2, 12, 10])).toBe(30);
+    });
+    test('return 0 if there are no multiple of 3 or 5 ', () => {
+        expect(sumMultiples([1, 4, 13.0, 2, 17, 11])).toBe(0);
+    });
+
+
+});
+describe("isValidDNA", () => {//C, G, T or A
+    test('it throws an error if not passed String ', () => {
+        expect(() => {
+            isValidDNA();
+        }).toThrow("str is required");
+    });
+    test("if the string is only letters from (C, G, T or A) , returns true", () => {
+        expect(isValidDNA("C")).toBe(true);
+        expect(isValidDNA("G")).toBe(true);
+        expect(isValidDNA("A")).toBe(true);
+        expect(isValidDNA("T")).toBe(true);
+        expect(isValidDNA("GTT")).toBe(true);
+        expect(isValidDNA("TAGGG")).toBe(true);
+        expect(isValidDNA("GGGG")).toBe(true);
+        expect(isValidDNA("AAAAT")).toBe(true);
+        expect(isValidDNA("CGTA")).toBe(true);
+
+    });
+    test("Should not be case sensitive", () => {
+        expect(isValidDNA("CGaaa")).toBe(true);
+        expect(isValidDNA("cgTa")).toBe(true);
+        expect(isValidDNA("cgTamM")).toBe(false);
+    });
+
+    test("Should return false ,  if any letter found other than (C, G, T or A) in the string", () => {
+        expect(isValidDNA("CGMT")).toBe(false);
+        expect(isValidDNA("GG7G")).toBe(false);
+        expect(isValidDNA("AA!AT")).toBe(false);
+    });
+    test("Should return false ,  if no letter found from (C, G, T or A) in the string ", () => {
+        expect(isValidDNA("lll")).toBe(false);
+    });
+});
+describe("getComplementaryDNA", () => {
+    test('it throws an error if not a valid String ', () => {
+        expect(() => {
+            getComplementaryDNA();
+        }).toThrow("str is required");
+        expect(() => {
+            getComplementaryDNA("NMNMN");
+        }).toThrow("str is not a valid DNA string");
+    });
+    test("return a valid basePair", () => {
+        expect(getComplementaryDNA("CGT")).toBe("GCA");
+        expect(getComplementaryDNA("G")).toBe("C");
+        expect(getComplementaryDNA("GAT")).toBe("CTA");
+    });
+    test("Should not be case sensitive", () => {
+        expect(getComplementaryDNA("cgt")).toBe("GCA");
+        expect(getComplementaryDNA("a")).toBe("T");
+        expect(getComplementaryDNA("gaT")).toBe("CTA");
+    });
+});
+describe("isItPrime", () => {
+    test('it throws an error if not a number ', () => {
+        expect(() => {
+            isItPrime();
+        }).toThrow("n is required");
+        expect(() => {
+            isItPrime(3.65);
+        }).toThrow("n is not an integer");
+        expect(() => {
+            isItPrime(-5);
+        }).toThrow("negative integers can not be prime");
+        expect(() => {
+            isItPrime(0);
+        }).toThrow("zero is not prime");
+        expect(() => {
+            isItPrime(1);
+        }).toThrow("1 is not prime");
+    });
+    test('should return true ,if it is a prime number', () => {
+        expect(isItPrime(3)).toBe(true);
+        expect(isItPrime(23)).toBe(true);
+    });
+    test('should return false ,if not a prime number', () => {
+        expect(isItPrime(6)).toBe(false);
+        expect(isItPrime(102)).toBe(false);
+    });
+});
+describe("createMatrix", () => {
+    test('it throws an error if not pass parameters ', () => {
+        expect(() => {
+            createMatrix();
+        }).toThrow("n is required");
+        expect(() => {
+            createMatrix(5);
+        }).toThrow("fill is required");
+    });
+    test('should return matrix ', () => {
+        //to solve serializes to the same string problem to make deep comparison
+        expect(createMatrix(3, 'fn')).toStrictEqual ([['fn', 'fn', 'fn'], ['fn', 'fn', 'fn'], ['fn', 'fn', 'fn']]);
+    });
+});
+describe("areWeCovered", () => {
+    test('it rthrow error  send undefined staff or day', () => {
+        expect(() => {
+            areWeCovered(areWeCovered());
+        }).toThrow("staff is required");
+        expect(() => {
+            areWeCovered(([{ name: "marwa", rota: ["Monday", "Tuesday"] }]));
+        }).toThrow("day is required");
+        
+        
+    });
+    test('it returns false , if  there are no  staff', () => {
+        expect(areWeCovered([], 'Saturday')).toBe(false);
+    });
+    test('it returns false , if  there are  staff but not schedual to work ', () => {
+        const staff = [
+            { name: "marwa", rota: ["Monday", "Tuesday"] },
+            { name: "adam", rota: ["Monday", "Tuesday"] },
+            { name: "Noha", rota: ["Monday", "Tuesday"] },
+            { name: "jess", rota: ["Monday", "Tuesday"] }
+        ];
+        expect(areWeCovered(staff, 'Saturday')).toBe(false);
+    });
+    test('it returns true , if  there are more than 3 staff  ', () => {
+        const staff = [
+            { name: "marwa", rota: ["Monday", "Tuesday"] },
+            { name: "adam", rota: ["Monday", "Tuesday"] },
+            { name: "Noha", rota: ["Monday", "Tuesday"] },
+            { name: "jess", rota: ["Monday", "Tuesday"] }
+        ];
+        expect(areWeCovered(staff, 'Monday')).toBe(true);
+    });
+});
